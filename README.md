9c19784a24e3a16564301fc5652879e1a84f0f72
Muris Ceman <murisceman@gmail.com>
2022-03-07 19:00:28 +0100

    Init

---
+++ README.md
@@ -0,0 +1,125 @@
+<p>
+  <a href="https://www.frontsail.com/#gh-light-mode-only" target="_blank">
+    <img src="https://brand.frontsail.com/logo-github-dark.svg" alt="A FrontSail project" width="120" height="80">
+  </a>
+  <a href="https://www.frontsail.com/#gh-dark-mode-only" target="_blank">
+    <img src="https://brand.frontsail.com/logo-github-light.svg" alt="A FrontSail project" width="120" height="80">
+  </a>
+</p>
+
+<p>
+  <a href="https://github.com/frontsail/x-sail/releases"><img src="https://img.shields.io/github/v/release/frontsail/x-sail?display_name=tag&style=flat-square" alt="Latest Release"></a>
+  <a href="https://github.com/frontsail/x-sail/blob/main/LICENSE"><img src="https://img.shields.io/github/license/frontsail/x-sail.svg?style=flat-square" alt="License"></a>
+</p>
+
+# @frontsail/alpine-sail
+
+This [Alpine](https://github.com/alpinejs/alpine) plugin adds the magics `$sail` and `$resail`, and the directive `x-sail` to the Alpine scope. Its purpose is to handle HTML router links for navigating app routes created in [FrontSail](https://www.frontsail.com).
+
+## Installation
+
+You can use alpine-sail by either including it from a `<script>` tag or installing it via NPM.
+
+> If you are building your app with FrontSail or its core library [Underscored](https://github.com/frontsail/underscored), do not manually install this plugin as it is already included with Underscored.
+
+### Via CDN
+
+Include the CDN build of this plugin as a `<script>` tag before Alpine's core JS file.
+
+```html
+<!-- Alpine plugins -->
+<script defer src="https://unpkg.com/@frontsail/alpine-sail@1.x.x/dist/alpine-sail.min.js"></script>
+
+<!-- Alpine core -->
+<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script
+```
+
+### Via NPM
+
+Install the plugin from NPM for use inside your bundle.
+
+```bash
+npm install @frontsail/alpine-sail
+```
+
+Then initialize it from your bundle.
+
+```js
+import Alpine from 'alpinejs'
+import sail from '@frontsail/alpine-sail'
+
+Alpine.plugin(sail)
+```
+
+## $sail
+
+The `$sail` magic is a direct wrapper for the `_D.sail` method from Underscored. It accepts a string, number, array of strings and numbers, or an object as an argument. When invoked, it attempts to resolve the routes for the specified path in the argument and change the current URL without reloading the page.
+
+```js
+$sail('/captains')
+// > http://localhost/captains
+
+$sail(1)
+// > http://localhost/captains/1
+
+$sail({ path: '..', state: { foo })
+// > http://localhost/captains
+
+$sail({ path: '/', replace: true })
+// > http://localhost
+
+history.back()
+// > http://localhost/captains/1
+```
+
+You can learn more about `_D.sail` on the official [documentation page](https://www.frontsail.com/docs/routing#sail).
+
+## $resail
+
+Just like the previous magic, `$resail` calls the Underscored method `_D.resail` directly, which replaces the current history state in the browser instead of pushing a new one. It accepts the same argument as `$sail`.
+
+## x-sail
+
+The `x-sail` directive can be used in HTML anchors in place of the standard `href` attribute, or in any other elements. When the element is clicked, it calls `_D.sail` with its evaluated attribute value as the method argument. A `href` attribute is automatically created and updated on every URL change if the directive is applied to an anchor.
+
+### Absolute and relative paths
+
+Paths beginning with a forward slash (`/`) are absolute and appended to the app's base URL.
+
+```js
+console.log(location.href, _D.base())
+// > http://localhost/app/captains, /app
+
+$sail('/settings')
+// > http://localhost/app/settings
+```
+
+All other paths are considered relative to the current URL.
+
+```js
+console.log(location.href, _D.base())
+// > http://localhost/app/captains, /app
+
+$sail('settings')
+// > http://localhost/app/captains/settings
+```
+
+### URLs
+
+If an internal URL is provided instead of the path, the app's base URL is automatically removed. When navigating within your application, always try to use a path and not the full URL.
+
+For external URLs, `x-sail` sets the default values of the `target` and `rel` attributes to `_blank` and `noopener noreferrer`, respectively. You can override this behavior by adding these attributes and specifying their values manually, or simply use the `href` attribute instead of `x-sail`.
+
+### Active classes
+
+You can automatically toggle CSS classes when a router link is active by adding the class names as dot separated modifiers.
+
+```html
+<a x-sail.underline.text-blue-400="'/'">Home</a>
+```
+
+Alternatively, you can pass the class names as an object parameter.
+
+```html
+<a x-sail="{ path: '/', active: 'underline text-blue-400' }">Home</a>
+```
---
+++ src/index.js
@@ -0,0 +1,158 @@
+export default function (Alpine) {
+  // The magic
+  Alpine.magic('sail', () => (link) => _D?.sail(link))
+
+  // The directive
+  Alpine.directive('sail', (el, { modifiers, expression }, { effect, evaluateLater, cleanup }) => {
+    const link = { path: null, state: null, replace: false, active: null }
+    const evaluator = evaluateLater(expression)
+    const active = []
+    const anchor = {}
+
+    // Listen...
+    effect(() => {
+      evaluator((evaluated) => {
+        // CSS classes
+        active.splice(0, active.length)
+        active.push(...modifiers)
+
+        if (typeof evaluated === 'string' || typeof evaluated === 'number') {
+          // The number zero should be
+          // trapped here, but why would
+          // anyone use 0 in an ID at all?
+          link.path = evaluated.toString()
+        } else if (!evaluated) {
+          return unlink()
+        } else if (Array.isArray(evaluated)) {
+          link.path = evaluated.join('/')
+        } else if (typeof evaluated === 'object') {
+          // Links can provided as objects, like:
+          // { path: '/sunrise', active: 'shadow' }
+          link.path = Array.isArray(evaluated.path)
+            ? evaluated.path.join('/')
+            : evaluated.path.toString()
+          link.state = evaluated.state ?? null
+          link.replace = !!evaluated.replace
+
+          if (evaluated.active) {
+            active.push(...evaluated.active.split(' ').filter(Boolean))
+          }
+        }
+
+        // No path,
+        // No sailing.
+        if (!link.path) {
+          return unlink()
+        }
+
+        // Reflect URL changes on
+        // active CSS classes and
+        // a[href] attributes.
+        if (active.length || el.tagName === 'A') {
+          refresh()
+          addEventListener('underscored:sailed', refresh)
+        }
+
+        // Trigger on...
+        el.addEventListener('click', click)
+      })
+    })
+
+    // Hard cleanup
+    cleanup(() => unlink())
+
+    /**
+     * Prevent the default click action and try navigating with `_D.sail()`.
+     * If the target element is an anchor with an external link, do nothing.
+     *
+     * @param {Event} event
+     */
+    function click(event) {
+      if (_D && !anchor.external) {
+        event.preventDefault()
+        _D.sail(link)
+      }
+    }
+
+    /**
+     * Toggle active CSS classes when the current link is active.
+     *
+     * If the target element is an anchor, generate a URL from the current path
+     * with `_D.url` and set it as its `href` attribute. Also, set the default
+     * values of `target` and `rel` to `_blank` and `noopener noreferrer`.
+     */
+    function refresh() {
+      // Sink...
+      if (!_D) {
+        return
+      }
+
+      // Create URL from path
+      const url = _D.url(link.path)
+
+      // Handle active CSS classes
+      if (url?.origin === location.origin && url?.pathname === location.pathname) {
+        active.forEach((className) => el.classList.add(className))
+      } else {
+        active.forEach((className) => el.classList.remove(className))
+      }
+
+      // Handle anchors
+      if (el.tagName === 'A') {
+        if (!anchor.initial) {
+          // Some attributes
+          // are restored
+          // upon cleaning.
+          anchor.initial = {
+            href: el.getAttribute('href'),
+            target: el.getAttribute('target'),
+            rel: el.getAttribute('rel')
+          }
+        }
+
+        anchor.url = url?.href ?? ''
+        anchor.external = !anchor.url.startsWith(location.origin + _D.base())
+        el.setAttribute('href', anchor.url)
+
+        if (anchor.external) {
+          if (typeof anchor.initial.target !== 'string') {
+            el.setAttribute('target', '_blank')
+          }
+
+          if (typeof anchor.initial.rel !== 'string') {
+            el.setAttribute('rel', 'noopener noreferrer')
+          }
+        } else {
+          restoreAnchorAttributes(['target', 'rel'])
+        }
+      }
+    }
+
+    /**
+     * Restore initial anchor attributes.
+     *
+     * @param {string[]} atts
+     */
+    function restoreAnchorAttributes(atts = ['href', 'target', 'rel']) {
+      if (anchor.initial) {
+        atts.forEach((att) => {
+          if (typeof anchor.initial[att] === 'string') {
+            el.setAttribute(att, anchor.initial[att])
+          } else {
+            el.removeAttribute(att, anchor.initial[att])
+          }
+        })
+      }
+    }
+
+    /**
+     * Soft cleanup.
+     */
+    function unlink() {
+      el.removeEventListener('click', click)
+      removeEventListener('underscored:sailed', refresh)
+      active.forEach((className) => el.classList.remove(className))
+      restoreAnchorAttributes()
+    }
+  })
+}
